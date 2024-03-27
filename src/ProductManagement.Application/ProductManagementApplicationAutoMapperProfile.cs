856126f7d14ecf41bef1175029bd140ac80d240a
machonky <dev@macrobert.com>
2024-03-27 10:12:41 +0800

    Update Automapper Profile

--- src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
+++ src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ProductManagement.Categories;
 using ProductManagement.Products;
 
 namespace ProductManagement;
@@ -11,5 +12,7 @@ public class ProductManagementApplicationAutoMapperProfile : Profile
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
         CreateMap<Product, ProductDto>();
+        CreateMap<CreateUpdateProductDto, Product>();
+        CreateMap<Category, CategoryLookupDto>();
     }
 }
