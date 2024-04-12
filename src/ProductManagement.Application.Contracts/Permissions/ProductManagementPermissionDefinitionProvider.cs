2dfb91015f5d98d989279e41c36a4442ecf513f0
machonky <dev@macrobert.com>
2024-04-12 11:50:20 +0800

    New Permissions defined

--- src/ProductManagement.Application.Contracts/Permissions/ProductManagementPermissionDefinitionProvider.cs
+++ src/ProductManagement.Application.Contracts/Permissions/ProductManagementPermissionDefinitionProvider.cs
@@ -8,17 +8,11 @@ public class ProductManagementPermissionDefinitionProvider : PermissionDefinitio
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        //Define your own permissions here. Example:
-        //myGroup.AddPermission(ProductManagementPermissions.MyPermission1, L("Permission:MyPermission1"));
-        var myGroup = context.AddGroup(
-            ProductManagementPermissions.GroupName,
-            L("ProductManagement"));
-        myGroup.AddPermission(
-            "ProductManagement.ProductCreation",
-            L("ProductCreation"));
-        myGroup.AddPermission(
-            "ProductManagement.ProductDeletion",
-            L("ProductDeletion"));
+        var g = context.AddGroup(ProductManagementPermissions.GroupName, L("ProductManagement"));
+        g.AddPermission(ProductManagementPermissions.Products.Create, L("ProductCreation"));
+        g.AddPermission(ProductManagementPermissions.Products.Read, L("ProductListing"));
+        g.AddPermission(ProductManagementPermissions.Products.Update, L("ProductEditing"));
+        g.AddPermission(ProductManagementPermissions.Products.Delete, L("ProductDeletion"));
     }
 
     private static LocalizableString L(string name)
--- src/ProductManagement.Application.Contracts/Permissions/ProductManagementPermissions.cs
+++ src/ProductManagement.Application.Contracts/Permissions/ProductManagementPermissions.cs
@@ -4,6 +4,12 @@ public static class ProductManagementPermissions
 {
     public const string GroupName = "ProductManagement";
 
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public static class Products
+    {
+        public const string Default = GroupName + nameof(Products);
+        public const string Create = nameof(Default) + "." + nameof(Create);
+        public const string Read = nameof(Default) + "." + nameof(Read);
+        public const string Update = nameof(Default) + "." + nameof(Update);
+        public const string Delete = nameof(Default) + "." + nameof(Delete);
+    }
 }
